8eb96463ad64daba6e6222114228a7f254d83774
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-03-11 01:09:37 +0200

    test

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Unitest module"""
+
+import json
+import unittest
+from models.base_model import BaseModel
+
+
+class Test_Base(unittest.TestCase):
+    """Defines Base class test cases"""
+
+if __name__ == '__main__':
+    unittest.main()
+
