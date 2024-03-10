daa6b58c85e6190bf5c82074e340aa4ef74682fa
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-03-10 23:57:43 +0200

    task 7

--- console.py
+++ console.py
@@ -37,7 +37,7 @@ class HBNBCommand(cmd.Cmd):
         elif args[0] not in HBNBCommand.__classes:
             print("** class doesn't exist **")
         else:
-            print(eval(args[0].id))
+            print(eval(args[0])().id)
             storage.save()
             
     def do_all(self,line):
