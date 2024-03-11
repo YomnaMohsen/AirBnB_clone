d401e26b5292652ef8f89b1034056093e8b13e1c
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-03-12 01:02:40 +0200

    test

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -8,19 +8,12 @@ from models.place import Place
 
 class Test_place(unittest.TestCase):
     """Defines place class test cases"""
-    
     def check_type(self):
         p = Place()
         self.assertIsInstance(p, Place)
+    
         
-    def check_args(self):
-        p = Place()
-        self.assertEqual(p.description, "")
-        self.assertEqual(p.city_id, "")
-        self.assertEqual(p.latitude, 0.0)
-        self.assertIsInstance(p.name, str)
             
-
 if __name__ == '__main__':
     unittest.main()
 
--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -3,12 +3,25 @@
 
 import json
 import unittest
-from models.base_model import BaseModel
+from models.state import State
 
 
-class Test_Base(unittest.TestCase):
-    """Defines Base class test cases"""
+class Test_state(unittest.TestCase):
+    """Defines state class test cases"""
+    def check_type(self):
+        s = State()
+        self.assertIsInstance(s, State)
+    
+    def test_args(self):
+        s1 = State()
+        self.assertEqual(s1.name, "")
+        
+    def test_set_args(self):
+         s1 = State()
+         s1.name = "morroco"
+         self.assertEqual(s1.name, "morroco")
+        
+            
 
 if __name__ == '__main__':
     unittest.main()
-
