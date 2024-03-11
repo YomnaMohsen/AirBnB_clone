ba1bb8dd5adbd3d54bd43007e052974102cac8c5
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-03-11 15:54:20 +0200

    fix

--- models/user.py
+++ models/user.py
@@ -11,8 +11,3 @@ class User(BaseModel):
     password = ""
     first_name = ""
     last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """Initializes user object
-        """
-        super().__init__(*args, **kwargs)
