2af926f83c84911ecd79ea403239710a3604c695
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-03-12 01:05:58 +0200

    place

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -11,6 +11,20 @@ class Test_place(unittest.TestCase):
     def check_type(self):
         p = Place()
         self.assertIsInstance(p, Place)
+        
+    def test_args(self):
+        p = Place()
+        self.assertEqual(p.name, "")
+        self.assertEqual(p.latitude, 0.0)
+        self.assertEqual(p.max_guest, 0)
+        self.assertIsInstance(p.amenity_ids, list)
+        
+    def test_set_args(self):
+         p = Place()
+         p.name = "garden hotel"
+         self.assertEqual(p.name, "garden hotel")
+         p.max_guest += 2
+         self.assertEqual(p.max_guest, 2)  
     
         
             
